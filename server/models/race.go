22d99ce203cdb36f9b7a34d52c16d771733756bf
Blair McKenzie <bmckenzie16@gmail.com>
2025-02-26 23:02:57 +0000

    Add model for race

---
+++ server/models/race.go
@@ -0,0 +1,17 @@
+package main
+
+import "time"
+
+type Race struct {
+	CircuitId 			int        
+	RaceId 				int
+	RaceName       		string 
+	CountryId        	int      
+	CountryCode      	string    
+	CountryName      	string    
+	OfficialRaceName 	string   
+	DateStart       	time.Time
+	CutoffTime      	time.Time
+	Year            	int
+	IsSprint 			bool
+}
